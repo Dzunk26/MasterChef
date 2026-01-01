7ef6814013f088ba11e809814436e1f74a843e90
Dzunk <dungpham20062004@gmail.com>
2026-01-01 16:52:01 +0700

    Create gameobject floor, Player, add movement animation, use input action to create script move, use cinemachine(camera)

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour {
+
+    private PlayerInputAction inputActions;
+
+    private void Awake() {
+        inputActions = new PlayerInputAction();
+        inputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
